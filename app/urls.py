63bb3d35999ae665fd2686f4f3b5d91b986028ef
tjiang11 <tjiang11@jhu.edu>
2015-06-14 09:15:38 -0400

    Added navbar linking to homepage and made homepage extended base

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import patterns, url
 from app import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='app'))
