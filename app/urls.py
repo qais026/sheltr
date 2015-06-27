db539300edeae157ba2a0cd650ade3e08313b58e
tjiang11 <tjiang11@jhu.edu>
2015-06-26 23:22:28 -0400

    Added more search result details and made left scrollpane for results

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import patterns, url
 from app import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='app'))
+        url(r'^/$', views.index, name='app'))
