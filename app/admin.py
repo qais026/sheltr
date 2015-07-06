738e7ed6246328aec0f1b82705c37373e4eff197
Channing Kimble-Brown <cnoellekb@gmail.com>
2015-07-05 21:32:50 -0400

    Added the ability to import categories (requires it's own csv file and an id column for now)

--- app/admin.py
+++ app/admin.py
@@ -3,25 +3,22 @@ from app.models import Provider, Category
 from import_export import resources, fields
 from import_export.admin import ImportExportModelAdmin
 
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Category
+
+class CategoryAdmin(ImportExportModelAdmin):
+    resource_class = CategoryResource
+    pass
+
 class ProviderResource(resources.ModelResource):
     class Meta:
-        model = Provider
-        # ESSENTIAL LINES BELOW THAT AREN'T MENTIONED IN THE TUTORIAL 
-        fields = ('id', 'provider_name', 'location_name', 'image', 'website', 'address1', 'address2', 'city', 'state', 'zipcode',)
+        model = Provider 
+        #fields = ('id', 'provider_name', 'location_name', 'image', 'website', 'address1', 'address2', 'city', 'state', 'zipcode', 'resources')
     
 class ProviderAdmin(ImportExportModelAdmin):
     resource_class = ProviderResource
     pass
 
-admin.site.register(Category)
-# Channing's comments:
-# Also essential that ProviderAdmin is added as a second argument
-# And in order to import csv there has to be an id column (I created
-# a version of the csv called SheltrProvidersID.csv that works)
-# also remember to add __str__ in order for names to show up on
-# the admin module thing.
-# MAIN ISSUE: Doesn't accept multiple providers of the same name
-# POSSIBLE SOLUTION: recreate sheltr database where unique is no
-# longer true for provider_name in models
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Provider, ProviderAdmin)
-# Register your models here.
--- app/models.py
+++ app/models.py
@@ -4,15 +4,19 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=30)
+    id = models.AutoField(unique=True, primary_key=True)
+    category_name = models.CharField(max_length=128, unique=True)
 
     def __unicode__(self):              
+        return self.category_name
+
+    def __str__(self):
         return self.category_name
 
 class Provider(models.Model):
     # blank=True necessary to allow for incomplete data entry edits
     # makes it so the field is NOT required
-    id = models.IntegerField(max_length=None, unique=True, primary_key=True)
+    id = models.AutoField(unique=True, primary_key=True)
     provider_name = models.CharField(max_length=128)
     location_name = models.CharField(max_length=128, null=True, blank=True)
     image = models.CharField(max_length=128, null=True, blank=True)
@@ -22,7 +26,18 @@ class Provider(models.Model):
     city = models.CharField(max_length=128, blank=True)
     state = models.CharField(max_length=128, blank=True)
     zipcode = models.CharField(max_length=128, blank=True)
-    categories = models.ManyToManyField(Category, blank=True)
+    """categories = models.ManyToManyField(Category, blank=True)     
+    contact = models.CharField(max_length=128, blank=True)
+    phone = models.CharField(max_length=128, blank=True)
+    hours = models.CharField(max_length=128, blank=True)
+    agency_description = models.CharField(max_length=128, blank=True)
+    eligibity = models.CharField(max_length=128, blank=True)
+    fee_structure_source = models.CharField(max_length=128, blank=True)
+    application_process = models.CharField(max_length=128, blank=True)
+    documents_required = models.CharField(max_length=128, blank=True)
+    details = models.CharField(max_length=128, blank=True)
+    details_continued = models.CharField(max_length=128, blank=True)
+    services = models.ForeignKey(Services, blank=True)"""
 
     def __unicode__(self):
         return self.provider_name
